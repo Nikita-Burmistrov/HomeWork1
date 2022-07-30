4831422ff9b84d3c6eb27fc257b7f1fa3a1a5d71
burmi@DESKTOP-EE7A2UG <burmistrov.03@mail.ru>
2022-07-30 15:21:06 +0300

    HomeWork1

---
+++ src/Exercise2.java
@@ -0,0 +1,11 @@
+import java.util.Arrays;
+
+public class Exercise2 {
+    public static void main(String[] args) {
+        int x=4500;
+        short y=4490;
+        byte z=1;
+        byte v= (byte) (x-y+z);
+        System.out.println("Ответ:"+ v);
+    }
+}
---
+++ src/Еxercise3.java
@@ -0,0 +1,11 @@
+public class Еxercise3 {
+    static int sum(int x,int y,int z){
+        return x * y - z;
+    }
+
+    public static void main(String[] args) {
+        int x=2,y=30,z=3;
+        int sum=sum(x,y,z);
+        System.out.println("Ответ:" +sum);
+    }
+}
